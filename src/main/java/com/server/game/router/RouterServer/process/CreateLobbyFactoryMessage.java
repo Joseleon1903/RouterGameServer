f4aeedb0034967ce96b6172fb328131953f42adb
jose eduardo <joseleon1903@outlook.com>
2020-10-06 14:51:30 -0400

    update code for use service in process method

--- src/main/java/com/server/game/router/RouterServer/process/CreateLobbyFactoryMessage.java
+++ src/main/java/com/server/game/router/RouterServer/process/CreateLobbyFactoryMessage.java
@@ -3,9 +3,9 @@ package com.server.game.router.RouterServer.process;
 import com.server.game.router.RouterServer.entity.Lobby;
 import com.server.game.router.RouterServer.entity.UserSession;
 import com.server.game.router.RouterServer.enums.UserType;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.client.RestTemplate;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import java.util.List;
 
 /**\
  *   FactoryMessage Detail for create a lobby
@@ -19,6 +19,8 @@ import org.springframework.web.client.RestTemplate;
  */
 public class CreateLobbyFactoryMessage extends FactoryMessage {
 
+    Logger logger = LoggerFactory.getLogger(CreateLobbyFactoryMessage.class);
+
     public CreateLobbyFactoryMessage(String[] data, String sessionId){
         this.data = data;
         this.sessionId = sessionId;
@@ -27,20 +29,22 @@ public class CreateLobbyFactoryMessage extends FactoryMessage {
     @Override
     public String process() throws Exception {
 
+        logger.info("Entering in method process");
+        logger.info("execute CreateLobbyFactoryMessage");
+        logger.info("session Id "+ sessionId);
+
         //validate is lobby alredy exist
         Boolean validLobby = false;
 
-        RestTemplate restTemplate = new RestTemplate();
-        String fooResourceUrl = "http://localhost:8080/lobbys";
-        ResponseEntity<Lobby[]> response = restTemplate.getForEntity(fooResourceUrl , Lobby[].class);
-
-        for (Lobby lb: response.getBody()) {
+        List<Lobby> response = lobbyService.avaliableLobby();
+        for (Lobby lb: response) {
             if(lb.getLobbyCode().equalsIgnoreCase(data[3])) {
                 validLobby = true;
             }
         }
 
         if(validLobby){
+            logger.info("Error lobby alredy exist");
             return "ERROR|201LB|ALREADYLOBBY";
         }
 
@@ -56,27 +60,17 @@ public class CreateLobbyFactoryMessage extends FactoryMessage {
         lb.setType(data[2]);
         lb.setSessionIdentifier(sessionId);
 
-        HttpEntity<Lobby> request = new HttpEntity<>(lb);
-        String cResourceUrl
-                = "http://localhost:8080/create/lobby";
-
-        restTemplate.postForObject(cResourceUrl, request, Lobby.class);
+        lobbyService.createGameLobby(lb);
 
         //update connection host of the server
+        UserSession userHost = clientService.findBySession(sessionId);
 
-        //UserSession sessionHost =
-        String getResourceUser = "http://localhost:8080/client?sessionId="+sessionId;
-        ResponseEntity<UserSession> getResponse = restTemplate.getForEntity(getResourceUser , UserSession.class);
-        UserSession userHost = getResponse.getBody();
         userHost.setPlayerName(UserType.LOBBYHOST.getType());
         userHost.setIsHost(true);
         userHost.setLobbyClient(data[3]);
 
-        String UpdateResourceUser = "http://localhost:8080/register/client";
-        HttpEntity<UserSession> requestUpdate = new HttpEntity<>(userHost);
-
-        restTemplate.postForObject(UpdateResourceUser, requestUpdate, UserSession.class);
-
+        clientService.registerClient(userHost);
+        logger.info("lobby successful create");
         return "CLIENT|201LB|OK";
     }
 }
